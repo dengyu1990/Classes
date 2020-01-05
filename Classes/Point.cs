15ab17b7e9feea24943cdb69f0f5b403978a01e4
Deng Yu <jardy@vip.qq.com>
2020-01-05 11:48:59 +0800

    添加项目文件。

---
+++ Classes/Point.cs
@@ -0,0 +1,41 @@
+﻿#region Using directives
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+#endregion
+
+namespace Classes
+{
+    class Point
+    {
+        private int x;
+        private int y;
+        public Point()
+        {
+            this.x = -1;
+            this.y = -1;
+        }
+        public Point(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+            Console.WriteLine($"x:{x},y:{y}");
+        }
+
+        public void Deconstruct(out int x, out int y)
+        {
+            x = this.x;
+            y = this.y;
+        }
+
+        public double DistanceTo(Point other)
+        {
+            int xDiff = this.x - other.x;
+            int yDiff = this.y - other.y;
+            return Math.Sqrt(xDiff * xDiff + yDiff * yDiff);
+        }
+
+    }
+}
---
+++ Classes/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    class Program
+    {
+        static void doWork()
+        {
+            Point ori = new Point();
+            (int x, int y) = ori;
+
+  
+            
+            Point xM = new Point(1366, 768);
+            double distance = xM.DistanceTo(ori);
+            Console.WriteLine($"Distance is:{distance}");
+        }
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                doWork();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
